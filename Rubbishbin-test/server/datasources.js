0f37bcebc5c517c9f8deedad18a00d7382084697
Rubbishbin <falvarado@nearbpo.com>
2017-08-29 07:13:36 -0500

    Use a datasource file in js extension

--- Rubbishbin-test/server/datasources.js
+++ Rubbishbin-test/server/datasources.js
@@ -1,14 +1,19 @@
 'use strict'
-
 require('dotenv').config()
 
-module.exports = {
-  mongodb: {
-    connector: 'mongodb',
-    hostname: process.env.DB_HOST,
-    port: process.env.DB_PORT || 27017,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'felipeprueba'
+module.exports = getDB();
+
+function getDB() {
+  const db = {
+    mlab: {
+      name: 'mlab',
+      connector: 'mongodb',
+      hostname: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: 'felipeprueba'
+    }
   }
+  return db;
 }
